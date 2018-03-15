6bd573044a2c64bf15154d0de3b850a372d36984
Asabulu International <t19960804@gmail.com>
2018-03-15 11:31:48 +0800

    Initial Commit

---
+++ TextRecognition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextRecognition
+//
+//  Created by Asabulu International on 2018/3/15.
+//  Copyright © 2018年 asa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
